da27d73534acaab877ba4cfb6bfa810d0e2425e6
MihailPetkov96 <mpetkov228@gmail.com>
2024-02-22 18:20:36 +0200

    Add jsx to General component

--- src/components/CvBuilder.jsx
+++ src/components/CvBuilder.jsx
@@ -1,6 +1,10 @@
+import General from "./General";
+
 function CvBuilder() {
     return (
-        <section className="cv-builder"></section>
+        <section className="cv-builder">
+            <General />
+        </section>
     )
 }
 
